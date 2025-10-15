f92f3edb5694a2c6127ab88faa0d9ac1deeedbb4
todays_sun_day <178suna@gmail.com>
2025-10-15 13:05:05 +0900

    feat: 문자열과 구분자 입력 받기
    입력 문자열이 비어있거나 null 일 경우 0을 반환함.
    숫자 하나만 입력될 경우 해당 숫자를 그대로 반환함.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,18 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("덧셈할 문자열을 입력해 주세요.")
+
+    // 기능 1 : 문자열과 구분자 입력 받기
+    val input = Console.readLine()
+
+    if (input == null || input.isEmpty()) {
+        println("결과 : 0")
+    }
+
+    input.toIntOrNull()?.let { number ->
+        println("결과 : $number")
+    }
 }
