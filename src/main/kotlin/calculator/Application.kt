a16885e57e5ea3039882c703834a9e288345ac59
todays_sun_day <178suna@gmail.com>
2025-10-16 08:17:52 +0900

    refactor: while break 문 개선 등
    기존의 while break 문을 삭제하고, 초반에 result 값을 선언하여, 경우에 따라 해당 result 값을 갱신하는 방식으로 변경함.
    == null && isEmpty() 코드에서 isNullOrEmpty() 코드로 변경함.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -3,47 +3,42 @@ package calculator
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    while (true){
-        println("덧셈할 문자열을 입력해 주세요.")
+    println("덧셈할 문자열을 입력해 주세요.")
+    var result = 0
 
-        // 기능 1 : 문자열과 구분자 입력 받기
-        val input = Console.readLine()
+    // 기능 1 : 문자열과 구분자 입력 받기
+    val input = Console.readLine()
 
-        if (input == null || input.isEmpty()) {
-            println("결과 : 0")
-            break
-        }
+    if (input.isNullOrEmpty()) {
+        result = 0
+    }
 
-        input.toIntOrNull()?.let { number ->
-            println("결과 : $number")
-            break
-        }
+    input.toIntOrNull()?.let { number ->
+        result = number
+    }
 
-        // 기능 2 : 구분자 분석 및 문자열 분리
-        var numbers : List<String>
-        var result = 0
+    // 기능 2 : 구분자 분석 및 문자열 분리
+    val numbers : List<String>
 
-        if (input.startsWith("//") &&
-            input.startsWith("\\n", startIndex = 3)){
-            val customDelimiter = input[2]
-            numbers = input.substring(5).split(",", ":", "$customDelimiter")
+    if (input.startsWith("//") &&
+        input.startsWith("\\n", startIndex = 3)){
+        val customDelimiter = input[2]
+        numbers = input.substring(5).split(",", ":", "$customDelimiter")
+    }
+    else{
+        numbers = input.split(",", ":")
+    }
+
+    // 기능 3 : 숫자 변환 및 합계 계산
+    // 기능 4 : 예외처리 (음수거나 구분된 문자열이 숫자가 아닐 시 오류)
+    for (number in numbers){
+        val num = number.toIntOrNull()
+        if (num != null && num >= 0){
+            result += num
         }
         else{
-            numbers = input.split(",", ":")
+            throw IllegalArgumentException()
         }
-
-        // 기능 3 : 숫자 변환 및 합계 계산
-        // 기능 4 : 예외처리 (음수거나 구분된 문자열이 숫자가 아닐 시 오류)
-        for (number in numbers){
-            val num = number.toIntOrNull()
-            if (num != null && num >= 0){
-                result += num
-            }
-            else{
-                throw IllegalArgumentException()
-            }
-        }
-        println("결과 : $result")
-        break
     }
+    println("결과 : $result")
 }
