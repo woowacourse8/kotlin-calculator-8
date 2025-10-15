73fc87fe5430853c55d4d912eee547f7c5345f67
todays_sun_day <178suna@gmail.com>
2025-10-15 19:17:27 +0900

    feat: 구분자 분석 및 문자열 분리
    기본 구분자(쉼표, 콜론)을 사용하여 문자열 분리함.
    커스텀 구분자(//와 \n 사이의 문자)를 인식하고, 이를 기준으로 문자열을 분리함.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -3,16 +3,35 @@ package calculator
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    println("덧셈할 문자열을 입력해 주세요.")
+    while (true){
+        println("덧셈할 문자열을 입력해 주세요.")
 
-    // 기능 1 : 문자열과 구분자 입력 받기
-    val input = Console.readLine()
+        // 기능 1 : 문자열과 구분자 입력 받기
+        val input = Console.readLine()
 
-    if (input == null || input.isEmpty()) {
-        println("결과 : 0")
-    }
+        if (input == null || input.isEmpty()) {
+            println("결과 : 0")
+            break
+        }
 
-    input.toIntOrNull()?.let { number ->
-        println("결과 : $number")
+        input.toIntOrNull()?.let { number ->
+            println("결과 : $number")
+            break
+        }
+
+        // 기능 2 : 구분자 분석 및 문자열 분리
+        var numbers : List<String>
+
+        if (input.startsWith("//") &&
+            input.startsWith("\\n", startIndex = 3)){
+            val customDelimiter = input[2]
+            numbers = input.split(",", ":", "$customDelimiter")
+
+
+        }
+        else{
+            numbers = input.split(",", ":")
+        }
+
     }
 }
