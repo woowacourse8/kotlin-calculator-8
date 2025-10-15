b288fa28b9cfe8780fc8b2c9703149118e50e40f
todays_sun_day <178suna@gmail.com>
2025-10-16 07:53:55 +0900

    fix: 커스텀 구분자 부분 수정
    기존 코드는 \n 뒤에 바로 숫자가 나올 경우 구분자로 문자열을 나올 때 앞 숫자가 숫자로 변환되지 않았음.
    따라서 커스텀 구분자를 위한 문자열들을 제외하는 코드를 추가함.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -21,17 +21,15 @@ fun main() {
 
         // 기능 2 : 구분자 분석 및 문자열 분리
         var numbers : List<String>
+        var result = 0
 
         if (input.startsWith("//") &&
             input.startsWith("\\n", startIndex = 3)){
             val customDelimiter = input[2]
-            numbers = input.split(",", ":", "$customDelimiter")
-
-
+            numbers = input.substring(5).split(",", ":", "$customDelimiter")
         }
         else{
             numbers = input.split(",", ":")
         }
-
     }
 }
