a6fdebe0982199c8c5a5776f81fc6f9d2c79bc9e
todays_sun_day <178suna@gmail.com>
2025-10-16 08:28:59 +0900

    fix: 추가로 고려되는 예외사항을 위한 수정
    customDelimiter 가 한 글자가 아닌 두 글자 이상일 수도 있는 경우를 위하여 코드를 수정함

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -4,33 +4,35 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
-    var result = 0
 
     // 기능 1 : 문자열과 구분자 입력 받기
     val input = Console.readLine()
 
     if (input.isNullOrEmpty()) {
-        result = 0
+        println("결과 : 0")
+        return
     }
 
     input.toIntOrNull()?.let { number ->
-        result = number
+        println("결과 : $number")
+        return
     }
 
     // 기능 2 : 구분자 분석 및 문자열 분리
-    val numbers : List<String>
-
-    if (input.startsWith("//") &&
-        input.startsWith("\\n", startIndex = 3)){
-        val customDelimiter = input[2]
-        numbers = input.substring(5).split(",", ":", "$customDelimiter")
+    val numbers : List<String> =
+    if (input.startsWith("//") && input.contains("\\n")){
+        val parts = input.split("\\n", limit = 2)
+        val customDelimiter = parts[0].substring(2)
+        parts[1].split(",", ":", customDelimiter)
     }
     else{
-        numbers = input.split(",", ":")
+        input.split(",", ":")
     }
 
     // 기능 3 : 숫자 변환 및 합계 계산
     // 기능 4 : 예외처리 (음수거나 구분된 문자열이 숫자가 아닐 시 오류)
+    var result = 0
+
     for (number in numbers){
         val num = number.toIntOrNull()
         if (num != null && num >= 0){
