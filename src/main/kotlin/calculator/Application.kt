abfaed08ab0268ab15e379603c8cd7ac8b71c1c0
todays_sun_day <178suna@gmail.com>
2025-10-19 16:30:16 +0900

    refactor: PR 체크리스트에 맞춰 코드 수정
    원시값 포장함, 숫자 목록과 관련된 행위를 캡슐화함, 한 메서드에 한 단계 들여쓰기함

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -3,44 +3,84 @@ package calculator
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    println("덧셈할 문자열을 입력해 주세요.")
+    val inputView = InputView()
+    val resultView = ResultView()
+    val inputParser = InputParser()
 
-    // 기능 1 : 문자열과 구분자 입력 받기
-    val input = Console.readLine()
+    val stringCalculator = StringCalculator(inputParser)
 
-    if (input.isNullOrEmpty()) {
-        println("결과 : 0")
-        return
+    val input = inputView.readInput()
+    val result = stringCalculator.add(input)
+    resultView.printResult(result)
+}
+
+// --- 입출력 ---
+class InputView {
+    fun readInput(): String? {
+        println("덧셈할 문자열을 입력해 주세요.")
+        return Console.readLine()
     }
+}
 
-    input.toIntOrNull()?.let { number ->
-        println("결과 : $number")
-        return
+class ResultView {
+    fun printResult(result: Int){
+        println("결과 : $result")
     }
+}
 
-    // 기능 2 : 구분자 분석 및 문자열 분리
-    val numbers : List<String> =
-    if (input.startsWith("//") && input.contains("\\n")){
-        val parts = input.split("\\n", limit = 2)
-        val customDelimiter = parts[0].substring(2)
-        parts[1].split(",", ":", customDelimiter)
-    }
-    else{
-        input.split(",", ":")
-    }
-
-    // 기능 3 : 숫자 변환 및 합계 계산
-    // 기능 4 : 예외처리 (음수거나 구분된 문자열이 숫자가 아닐 시 오류)
-    var result = 0
-
-    for (number in numbers){
-        val num = number.toIntOrNull()
-        if (num != null && num >= 0){
-            result += num
+// --- 문자열 계산기 ---
+class StringCalculator(private val parser: InputParser) {
+    fun add(input: String?): Int {
+        if (input.isNullOrEmpty()) {
+            return 0
         }
-        else{
-            throw IllegalArgumentException()
+
+        input.toIntOrNull()?.let {
+            return PositiveNumber(it).value
         }
+
+        val numbers = parser.parse(input)
+
+        return numbers.sum()
     }
-    println("결과 : $result")
+}
+
+// --- 문자열 파서 ---
+class InputParser {
+    private val customDelimiterPrefix = "//"
+    private val customDelimiterSuffix = "\\n"
+
+    fun parse(input: String): Numbers {
+        val stringNumbers = splitInput(input)
+
+        val positiveNumbers = stringNumbers.map {
+            val num = it.toIntOrNull()
+                ?: throw IllegalArgumentException()
+            PositiveNumber(num)
+        }
+        return Numbers(positiveNumbers)
+    }
+
+    private fun splitInput(input: String): List<String> {
+        if (input.startsWith(customDelimiterPrefix) && input.contains(customDelimiterSuffix)){
+            val parts = input.split(customDelimiterSuffix, limit = 2)
+            val customDelimiter = parts[0].substring(customDelimiterPrefix.length)
+            return parts[1].split(",", ":", customDelimiter)
+        }
+
+        return input.split(",", ":")
+    }
+}
+
+@JvmInline
+value class PositiveNumber(val value: Int) {
+    init {
+        require(value >= 0)
+    }
+}
+
+class Numbers(private val values: List<PositiveNumber>) {
+    fun sum(): Int {
+        return values.sumOf { it.value }
+    }
 }
